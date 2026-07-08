0c631d70cc759b0ec7896abf872f843493919c60
ALOKKUMAR3129 <alokkumar3706@gmail.com>
2026-07-08 21:10:37 +0530

    JAVA

---
+++ src/BASIC/simpleofinterest.java
@@ -0,0 +1,19 @@
+package BASIC;
+
+import java.util.Scanner;
+
+public class simpleofinterest {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("ENTER PRINCIPAL = ");
+        double p = sc.nextDouble();
+        System.out.print("ENTER RATE = ");
+        double r = sc.nextDouble();
+        System.out.print("ENTER TIME = ");
+        double t = sc.nextDouble();
+        double si = (p*r*t)/100;
+        System.out.println("SIMPLE INTEREST = " + si);
+        sc.close();
+
+    }
+}
