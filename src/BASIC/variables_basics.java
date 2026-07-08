6617b0cbfe06b7d1a1cc54f16618a92e0fa823f8
ALOKKUMAR3129 <alokkumar3706@gmail.com>
2026-07-08 19:29:52 +0530

    JAVA

---
+++ src/BASIC/variables_basics.java
@@ -0,0 +1,27 @@
+package BASIC;
+
+public class variables_basics {
+    static void main(String[] args) {
+        int x = 5;
+        System.out.println(x);
+        int y;
+        y = 10;
+        System.out.println(y);
+        System.out.println(x*y);
+        x = 7;
+        System.out.println(x*y);
+        y = 3;
+        System.out.println(x*y);
+        x = x*2;
+        System.out.println(x);
+        x = x+2;
+        System.out.println(x);
+        x = x-2;
+        System.out.println(x);
+        x = x/2;
+        System.out.println(x);
+
+
+    }
+
+}
