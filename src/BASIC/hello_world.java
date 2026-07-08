c8028213c809bba9a3aa6867b89525941e776f76
ALOKKUMAR3129 <alokkumar3706@gmail.com>
2026-07-08 19:06:42 +0530

    BASICS

--- src/BASIC/hello_world.java
+++ src/BASIC/hello_world.java
@@ -3,5 +3,6 @@ package BASIC;
 public class hello_world {
     public static void main(String[] args) {
         System.out.println("Hello world");
+
     }
 }
--- src/BASIC/printing_number.java
+++ src/BASIC/printing_number.java
@@ -4,6 +4,7 @@ public class printing_number {
     static void main(String[] args) {
         System.out.println(69+1);
         System.out.println("69+1");
+
     }
 
 }
