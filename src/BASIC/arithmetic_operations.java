092258b5b40b92ea872e4c2f7ea0b95450039967
ALOKKUMAR3129 <alokkumar3706@gmail.com>
2026-07-08 19:40:48 +0530

    JAVA

---
+++ src/BASIC/arithmetic_operations.java
@@ -0,0 +1,21 @@
+package BASIC;
+
+public class arithmetic_operations {
+    static void main(String[] args) {
+        int x = 5, y = 3;
+        System.out.println(x + y);
+        System.out.println(x - y);
+        System.out.println(x * y);
+        System.out.println(x / y);
+
+        System.out.println("-----------");
+
+        double a = 5, b = 3;
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+
+
+    }
+}
