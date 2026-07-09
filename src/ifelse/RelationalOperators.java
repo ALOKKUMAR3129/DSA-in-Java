495535b7e75b166a92e6f7ff6664df8ec66df3be
ALOKKUMAR3129 <alokkumar3706@gmail.com>
2026-07-09 13:18:14 +0530

    JAVA

---
+++ src/ifelse/RelationalOperators.java
@@ -0,0 +1,33 @@
+package ifelse;
+
+public class RelationalOperators {
+    public static void main(String[] args) {
+        System.out.println(5>3);
+        System.out.println(5<3);
+        System.out.println(5==3);
+        System.out.println(5!=3);
+        System.out.println(5>=3);
+        System.out.println(5<=3);
+
+        System.out.println("----------");
+
+        boolean x = (5>3);
+        System.out.println(x);
+
+        boolean y = (5<3);
+        System.out.println(y);
+
+        boolean z = (5==3);
+        System.out.println(z);
+
+        boolean w = (5!=3);
+        System.out.println(w);
+
+        boolean a = (5>=3);
+        System.out.println(a);
+
+        boolean b = (5<=3);
+        System.out.println(b);
+
+    }
+}
