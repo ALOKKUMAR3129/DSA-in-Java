b06f8f67be4a643cee3c31e56756107630e6a130
ALOKKUMAR3129 <alokkumar3706@gmail.com>
2026-07-09 19:48:30 +0530

    JAVA

---
+++ src/ifelse/DivisibleBy5.java
@@ -0,0 +1,19 @@
+package ifelse;
+
+import java.util.Scanner;
+
+public class DivisibleBy5 {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number = ");
+        int n = sc.nextInt();
+        if (n % 5 == 0) {
+            System.out.println("Divisible By 5");
+        }
+        else{
+            System.out.println("Not Divisible By 5");
+        }
+        sc.close();
+    }
+}
+
